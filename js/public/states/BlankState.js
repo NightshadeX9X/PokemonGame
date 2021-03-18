9313f9936809f498015e5af4dd212c44c5acd635
NightshadeX9X <solgaliumz@gmail.com>
2021-03-18 08:50:11 +0100

    deleted blankstate because you can just use state instead

--- js/public/states/BlankState.js
+++
@@ -1,9 +0,0 @@
-import State from "../core/State.js";
-var BlankState = /** @class */ (function () {
-    function BlankState(stateStack) {
-        this.stateStack = stateStack;
-        State.call(this, stateStack);
-    }
-    return BlankState;
-}());
-export default BlankState;
--- ts/public/states/RoamState.ts
+++ ts/public/states/RoamState.ts
@@ -1,21 +1,64 @@
+import Input from "../core/Input.js";
+import Loader from "../core/Loader.js";
 import State from "../core/State.js";
 import StateStack from "../core/StateStack.js";
 import { ChildOf } from "../util/functions.js";
+import Vector from "../util/Vector.js";
 
-interface BlackScreenState extends State { }
-
+interface RoamState extends State { }
 @ChildOf(State)
-class BlackScreenState {
+class RoamState {
+	public tileSize = 16;
+
+	public colorToneMaxAlpha = 0.4;
+	/** The color tone overlay displayed on top of the Camera display. The color varies depending on the hour
+	 * This list provides all the color tones using an array, from hour 0 (00:00) to hour 23 (23:00)
+	 * The current time is rounded to the nearest hour, and that index of the this array is the color tone to draw.
+	 * Color tones should only be rendered in outdoor maps.
+	 * Format: [Red, Green, Blue, Alpha]
+	*/
+	public colorTones: [number, number, number, number][] = [
+		[0, 0, 255, this.colorToneMaxAlpha], // 0
+		[0, 0, 255, this.colorToneMaxAlpha * 0.8], // 1
+		[0, 0, 255, this.colorToneMaxAlpha * 0.6], // 2
+		[0, 0, 255, this.colorToneMaxAlpha * 0.4], // 3
+		[0, 0, 255, this.colorToneMaxAlpha * 0.2], // 4
+		[0, 0, 255, 0], // 5
+		[255, 255, 0, 0], // 6
+		[255, 255, 0, this.colorToneMaxAlpha * 0.2], // 7
+		[255, 255, 0, this.colorToneMaxAlpha * 0.4], // 8
+		[255, 255, 0, this.colorToneMaxAlpha * 0.6], // 9
+		[255, 255, 0, this.colorToneMaxAlpha * 0.8], // 10
+		[255, 255, 0, this.colorToneMaxAlpha], // 11
+		[255, 255, 0, this.colorToneMaxAlpha], // 12
+		[255, 255, 0, this.colorToneMaxAlpha * 0.8], // 13
+		[255, 255, 0, this.colorToneMaxAlpha * 0.6], // 14
+		[255, 255, 0, this.colorToneMaxAlpha * 0.4], // 15
+		[255, 255, 0, this.colorToneMaxAlpha * 0.2], // 16
+		[255, 255, 0, 0], // 17
+		[0, 0, 255, 0], // 18
+		[0, 0, 255, this.colorToneMaxAlpha * 0.2], // 19
+		[0, 0, 255, this.colorToneMaxAlpha * 0.4], // 20
+		[0, 0, 255, this.colorToneMaxAlpha * 0.6], // 21
+		[0, 0, 255, this.colorToneMaxAlpha * 0.8], // 22
+		[0, 0, 255, this.colorToneMaxAlpha], // 23
+	]
+
 	constructor(public stateStack: StateStack) {
 		State.call(this, stateStack);
+
 	}
 
+	public async preload(loader: Loader) { }
+	public update(input: Input) {
+		this.subStateStack.update(input);
+		this.backgroundProcesses.update(input);
+	}
 	public render(ctx: CanvasRenderingContext2D) {
-		ctx.save();
-		ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		ctx.restore();
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+		State.prototype.render.call(this, ctx);
 	}
 }
 
-export default BlackScreenState;
+export default RoamState;
