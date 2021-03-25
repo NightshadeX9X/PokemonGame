8035ef186049b149beff22ed86638face230a3b1
NightshadeX9X <solgaliumz@gmail.com>
2021-03-25 22:19:50 +0100

    added GameObjects. fixes #6

--- ts/public/roam_state/game_objects/definitions/route5/ladder1.ts
+++ ts/public/roam_state/game_objects/definitions/route5/ladder1.ts
@@ -5,6 +5,7 @@ import Ladder from "../../types/Ladder.js";
 class ladder1 extends Ladder {
 	constructor(roamState: RoamState) {
 		super(roamState, new Vector(3, 19), new Vector(2), 2, 1);
+
 	}
 }
 
